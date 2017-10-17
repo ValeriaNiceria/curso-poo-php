97bece265461b465a02a9be59e0fd6536eb36eec
ValeriaNiceria <valerianiceria@gmail.com>
2017-10-17 19:53:36 -0200

    Projeto Final em PHP (Parte 1)

--- aula-11/index.php
+++ aula-11/index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -21,11 +21,9 @@
             require_once 'Bolsista.php';
             require_once 'Tecnico.php';
             require_once 'Professor.php';
-
             $p[0] = new Visitante ("Joaquim ",35,"M");
             print_r($p[0]);
             $p[0]->fazerAniv();
-
             $p[1] = new Aluno("Gabriela", 15, "F",1221,"Jogos");
             print_r($p[1]);
             $p[1]->pagarMensalidade();
@@ -35,15 +33,12 @@
             $p[2]->renovarBolsa();
             $p[2]->pagarMensalidade();
             $p[2]->fazerAniv();
-
             $p[3] = new Tecnico ("Samuel", 18, "M", 1111, "Design",false);
             print_r($p[3]);
             $p[3]->praticar();
-
             $p[4] = new Professor("Mônica", 40, "F", "Arte",980.30);
             print_r($p[4]);
             $p[4]->receberAumento(300.10);
-
           
         ?>
     </pre>
